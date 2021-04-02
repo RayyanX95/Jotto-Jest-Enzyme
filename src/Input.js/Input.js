74c9e820d774938a8039f9e055a6c51fdfcee634
Ibrahim AlRayyan <ibrahim.alrayany@gmail>
2021-04-02 11:48:10 +0200

    test input  element

--- src/Input.js/Input.js
+++ src/Input.js/Input.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Input = ({  }) => {
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState("")
   return (
     <div data-test="component-input" >
-
+      <from className="form-inline" >
+        <input
+          data-test="input-box"
+          type="text"
+          className="mb-2 mx-sm-3"
+          placeholder="Enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          onClick={(e) => {
+            e.preventDefault();
+            // TODO: Update guessWords
+            // TOD: check against secret secretWord and update success 
+            setCurrentGuess("");
+          }}
+          className="btn btn-primary mb-2"
+        >Submit</button>
+      </from>
     </div>
   )
 }
--- src/Input.js/Input.test.js
+++ src/Input.js/Input.test.js
@@ -1,15 +1,55 @@
 import React from 'react'
 import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../../test/testUtils';
-import Input from './Input'
+import Input from './Input';
 
-const setup = (secretWord= "party") => shallow(<Input secretWord={secretWord}/>);
+// ==> mock entire module for structuring useState on import //
+// const mockSetCurrentGuess = jest.fn();
+// jest.mock('react', () => ({
+// 	...jest.requirActual('react'),
+// 	useState: (initialState) => [initialSate, mockCurrentGuess]
+// }));
+
+const setup = (secretWord = "party") => shallow(<Input secretWord={secretWord} />);
 
 it('render without error', () => {
   const wrapper = setup();
   const inputContainer = findByTestAttr(wrapper, 'component-input');
   expect(inputContainer.length).toBe(1);
 });
+
 test('does not throw warning with expected props', () => {
-  checkProps(Input, {secretWord: 'party'})
+  checkProps(Input, { secretWord: 'party' })
 })
+
+describe('state controlled input field', () => {
+  let mockSetCurrentGuess = jest.fn(); // Create mock function
+  let wrapper;
+  let originalUseState;
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    originalUseState = React.useState;
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  })
+  afterEach|(() => {
+    React.useState = originalUseState;
+  });
+
+  test('state updates with value of input box upon change', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    // The effect of these two line is simulating input box getting a value of `train`
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('Clear state when Submit button clicked', () => {
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    // This Line simulate the 'Submit' button get clicked
+    submitButton.simulate('click', {preventDefault(){}});
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  })
+})
+
+// 61
