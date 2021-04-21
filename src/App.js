16acae614e403a36a210dc2b256dc1cb78e7fa52
Ibrahim <ibrahim.alrayany@gmail.com>
2021-04-21 22:35:14 +0200

    mock changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import './App.css';
 import Congrats from './Congrats'
 import GuessedWords from './GuessedWords';
 import Input from './Input/Input';
+import { getSecretWord } from './actions'
 
 const App = () => {
   // TODO: get props from shared state
   const success = false;
   const secretWord = 'party';
   const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, [])
 
     return (
       <div data-test="component-app" className="container">
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,40 @@
 import React from 'react'
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
 import { findByTestAttr } from '../test/testUtils'
 
-const setup = () => shallow(<App />);
+
+// Activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
+
+// Sive we have done the last `mock` this import will be from __mocks__ folder
+import { getSecretWord as mockGetSecretWord } from './actions'
+
+// use mount, as useEffect not called on `shallow`
+// https://github.com/airbnb/issues/2086
+const setup = () => mount(<App />);
 
 test('renders without errors', () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent).toHaveLength(1);
 });
+
+describe('get Secret Word', () => {
+  beforeEach(() => {
+    // clear the mock calls from previous tests
+    mockGetSecretWord.mockClear();
+  })
+  test('getSecretWord on app mount', () => {
+    const wrapper = setup();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    // using setProps as wrapper.update() doesn't trigger useEffect
+    wrapper.setProps();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
+})
